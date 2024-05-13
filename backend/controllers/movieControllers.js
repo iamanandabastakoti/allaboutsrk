12dd6fdd9c99331185e7e18a120260c74931fa9f
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2024-05-13 21:38:51 +0545

    added controller to get the details about a single-movie

--- backend/controllers/movieControllers.js
+++ backend/controllers/movieControllers.js
@@ -40,4 +40,18 @@ const addMovie = async (req, res) => {
   }
 };
 
-module.exports = { getAllMovies, addMovie };
+const getSingleMovie = async (req, res) => {
+  try {
+    const { movieID } = req.params;
+    const oldMovie = await Movie.findById({ _id: movieID });
+    res.status(200).json(oldMovie);
+    console.log("Movie found");
+  } catch (error) {
+    console.log(error);
+    res
+      .status(400)
+      .json({ message: "Unable to find the movie of the given movieID" });
+  }
+};
+
+module.exports = { getAllMovies, addMovie, getSingleMovie };
--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAllMovies, addMovie } = require("../controllers/movieControllers");
+const {
+  getAllMovies,
+  addMovie,
+  getSingleMovie,
+} = require("../controllers/movieControllers");
 
 //defining the endpoints
 router.get("/allmovies", getAllMovies);
 router.post("/addmovie", addMovie);
+router.get("/singlemovie/:movieID", getSingleMovie);
 
 module.exports = router;
