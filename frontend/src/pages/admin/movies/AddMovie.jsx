04ed91edd0a95dfa70d76e998fc775cb222da73c
Ananda Bastakoti <105543272+iamanandabastakoti@users.noreply.github.com>
2024-05-15 20:47:15 +0545

    created function for adding multiple data of cast,genre,etc.

--- frontend/src/pages/admin/movies/AddMovie.jsx
+++ frontend/src/pages/admin/movies/AddMovie.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RxCross1 } from "react-icons/rx";
 import { IoMdAdd } from "react-icons/io";
 import { MdOutlineDelete } from "react-icons/md";
@@ -43,6 +43,26 @@ const AddMovie = ({ setAddMovieDialog }) => {
         }
     ];
 
+    const [castData, setCastData] = useState([{ name: '', image: '', role: '' }]);
+    const addMoreCast = () => {
+        setCastData([...castData, { castName: '', castPhoto: '', characterName: '' }])
+    }
+
+    const [genreData, setGenreData] = useState([{ name: '' }])
+    const addMoreGenre = () => {
+        setGenreData([...genreData, { name: '' }]);
+    }
+
+    const [producerData, setProducerData] = useState([{ name: '' }])
+    const addMoreProducer = () => {
+        setProducerData([...producerData, { name: '' }]);
+    }
+
+    const [productionData, setProductionData] = useState([{ name: '' }])
+    const addMoreProduction = () => {
+        setProductionData([...productionData, { name: '' }]);
+    }
+
     return (
         <>
             <div
@@ -73,18 +93,24 @@ const AddMovie = ({ setAddMovieDialog }) => {
                             <label className='text-lg' htmlFor="">Director:</label>
                             <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the movie director name' />
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col gap-1'>
                             <label className='text-lg' htmlFor="">Cast:</label>
-                            <div className='flex items-center gap-2'>
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Cast name' />
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Cast photo' />
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Character name' />
-                                <span>
-                                    <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
-                                </span>
-                            </div>
+                            {
+                                castData.map((cast) => {
+                                    return (
+                                        <div className='flex items-center gap-2'>
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Cast name' value={cast.name} />
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Cast photo' value={cast.image} />
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Character name' value={cast.role} />
+                                            <span>
+                                                <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
                             <span className='flex justify-end py-1'>
-                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' onClick={addMoreCast} />
                             </span>
                         </div>
                         <div className='flex flex-col'>
@@ -108,40 +134,58 @@ const AddMovie = ({ setAddMovieDialog }) => {
                             <label className='text-lg' htmlFor="">Runtime(in minutes):</label>
                             <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the runtime in minutes' />
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col gap-1'>
                             <label className='text-lg' htmlFor="">Genre:</label>
-                            <div className='flex items-center gap-2'>
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the genre' />
-                                <span>
-                                    <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
-                                </span>
-                            </div>
+                            {
+                                genreData.map((genre) => {
+                                    return (
+                                        <div className='flex items-center gap-2'>
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the genre' value={genre.name} />
+                                            <span>
+                                                <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
                             <span className='flex justify-end py-1'>
-                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' onClick={addMoreGenre} />
                             </span>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col gap-1'>
                             <label className='text-lg' htmlFor="">Producer:</label>
-                            <div className='flex items-center gap-2'>
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the producer name' />
-                                <span>
-                                    <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
-                                </span>
-                            </div>
+                            {
+                                producerData.map((producer) => {
+                                    return (
+                                        <div className='flex items-center gap-2'>
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the producer name' value={producer.name} />
+                                            <span>
+                                                <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
                             <span className='flex justify-end py-1'>
-                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' onClick={addMoreProducer} />
                             </span>
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col gap-1'>
                             <label className='text-lg' htmlFor="">Production Company:</label>
-                            <div className='flex items-center gap-2'>
-                                <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the production company name' />
-                                <span>
-                                    <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
-                                </span>
-                            </div>
+                            {
+                                productionData.map((production) => {
+                                    return (
+                                        <div className='flex items-center gap-2'>
+                                            <input className='px-3 py-2 bg-gray-100 rounded-lg focus:outline-none w-full' type="text" placeholder='Enter the production company name' value={production.name} />
+                                            <span>
+                                                <MdOutlineDelete className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                            </span>
+                                        </div>
+                                    )
+                                })
+                            }
                             <span className='flex justify-end py-1'>
-                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' />
+                                <IoMdAdd className='bg-buttonColor text-primaryBg p-1 text-3xl rounded-lg cursor-pointer hover:brightness-90' onClick={addMoreProduction} />
                             </span>
                         </div>
                         <div className='flex justify-center gap-6 items-center'>
