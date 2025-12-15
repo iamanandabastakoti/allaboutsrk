ac11764e60fcc7dd5a531f05d59ae4c53bb34a7c
Ananda Bastakoti <iamanandabastakoti@gmail.com>
2025-12-15 09:38:39 +0545

    updating react versions

--- frontend/src/pages/More.jsx
+++ frontend/src/pages/More.jsx
@@ -42,15 +42,15 @@ const More = () => {
         <form className='flex flex-col gap-3'>
           <div>
             <label className='px-2' htmlFor="">Name</label>
-            <input name='name' id='name' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none' type="text" placeholder='Enter your name' required value={name} onChange={e => setName(e.target.value)} />
+            <input name='name' id='name' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none' type="text" placeholder='Enter your name' value={name} onChange={e => setName(e.target.value)} />
           </div>
           <div>
             <label className='px-2' htmlFor="">Email</label>
-            <input name='email' id='email' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none' type="email" placeholder='Enter your email' required value={email} onChange={e => setEmail(e.target.value)} />
+            <input name='email' id='email' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none' type="email" placeholder='Enter your email' value={email} onChange={e => setEmail(e.target.value)} />
           </div>
           <div>
             <label className='px-2' htmlFor="">Message</label>
-            <textarea name='message' id='message' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none resize-none' cols="30" rows="4" placeholder='Enter your message' required value={message} onChange={e => setMessage(e.target.value)} />
+            <textarea name='message' id='message' className='border-2 border-borderColor rounded-lg p-2 w-full focus:outline-none resize-none' cols="30" rows="4" placeholder='Enter your message' value={message} onChange={e => setMessage(e.target.value)} />
           </div>
         </form>
         <button name='send-message-button' id='send-message-button' className='w-full bg-buttonColor p-2 rounded-lg flex justify-center items-center gap-1 text-lg hover:brightness-90' onClick={sendMessage}>
